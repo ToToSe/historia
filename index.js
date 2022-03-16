e26203cb01497f64eed984c61a3834c217231c97
Sofiène <sofiene@bob-desk.fr>
2022-03-16 20:21:53 +0100

    Cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const request = require('request');
 const app = express()
+const cors = require('cors')
 const cheerio = require('cherio')
 const Period = require('./period')
 
@@ -50,20 +51,19 @@ request('http://geacron.com/map/atlas/mapal.html?lang=fr', (error, response, bod
   </script>`);
 })
 
+app.use(cors({
+  exposedHeaders: ["Link"],
+  origin: [
+    'https://historia-io.herokuapp.com',
+    'http://historia-io.herokuapp.com',
+    'http://localhost:3000',
+  ],
+  methods: ['OPTIONS', 'GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+  credentials: true
+}))
+
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
